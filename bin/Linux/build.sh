926a3758d86ed97d5a8c823708bde9bc343f0376
scrat98 <artemgolovko98@gmail.com>
2018-02-01 00:49:10 +0300

    add build system

---
+++ bin/Linux/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Build for Linux
+
+case "$1" in
+"algorithm" | "algo")
+    pyinstaller ../../algorithm.py -F
+    ;;
+"visualizer" | "vis")
+    pyinstaller ../../visualizer.py -F
+    ;;
+"gui")
+    pyinstaller ../../gui.py -F
+    ;;
+"all")
+    pyinstaller ../../algorithm.py -F
+    pyinstaller ../../visualizer.py -F
+    pyinstaller ../../gui.py -F
+    ;;
+"help" | "h")
+    echo -e "Build options:
+    algorithm or algo   Build algorithm
+    visualizer or vis   Build visualizer
+    gui                 Build gui
+    help or h           Show help"
+    ;;
+*)
+    echo "Options are incorrect. Use h for help"
+    ;;
+esac
---
+++ bin/Windows/build.bat
@@ -0,0 +1,32 @@
+:: Build for Windows
+
+IF "%1"=="algorithm" pyinstaller ../../algorithm.py -F
+IF "%1"=="algo"      pyinstaller ../../algorithm.py -F
+
+IF "%1"=="visualizer" pyinstaller ../../visualizer.py -F
+IF "%1"=="vis"        pyinstaller ../../visualizer.py -F
+
+IF "%1"=="gui" pyinstaller ../../gui.py -F
+
+IF "%1"=="all" (
+    pyinstaller ../../algorithm.py -F
+    pyinstaller ../../visualizer.py -F
+    pyinstaller ../../gui.py -F
+)
+
+IF "%1"=="help" (
+    echo "Build options:"
+    echo "  algorithm or algo   Build algorithm"
+    echo "  visualizer or vis   Build visualizer"
+    echo "  gui                 Build gui"
+    echo "  help or h           Show help"
+)
+IF "%1"=="h" (
+    echo "Build options:"
+    echo "  algorithm or algo   Build algorithm"
+    echo "  visualizer or vis   Build visualizer"
+    echo "  gui                 Build gui"
+    echo "  help or h           Show help"
+) ELSE (
+    echo "Options are incorrect. Use h for help"
+)
