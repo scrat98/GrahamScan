d23996c12a63f6fe7727d00a05241cb7ce960e84
scrat98 <artemgolovko98@gmail.com>
2018-02-01 12:38:16 +0300

    add temp ui files

--- graham/gui.py
+++ graham/gui.py
@@ -1,9 +1,14 @@
+import sys, os
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5 import uic
+
+this_dir, gui_filename = os.path.split(__file__)
+ui_path = os.path.join(this_dir, "ui", "gui.ui")
 
 
 class GUIWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        # uic.loadUi('GrahamGUI.ui', self)
+        uic.loadUi(ui_path, self)
         print('GrahamGUI init success')
--- graham/visualizer.py
+++ graham/visualizer.py
@@ -1,11 +1,14 @@
-import sys
+import sys, os
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
+
+this_dir, this_filename = os.path.split(__file__)
+ui_path = os.path.join(this_dir, "ui", "visualizer.ui")
 
 
 class VisualizerWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        # uic.loadUi('GrahamVisualizer.ui', self)
+        uic.loadUi(ui_path, self)
         print('GrahamVisualizer init success')
