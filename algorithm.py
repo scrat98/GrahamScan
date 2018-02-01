700c403379f9b1d6510a4c347dea2a7c787022b8
scrat98 <artemgolovko98@gmail.com>
2018-02-01 20:18:41 +0300

    add algorithm example

--- algorithm.py
+++ algorithm.py
@@ -5,6 +5,10 @@ from graham.algorithm import GrahamAlgorithm
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    gui = GrahamAlgorithm()
+    algo = GrahamAlgorithm()
+    points = [[3, 9], [8, 5], [11, 1], [1, 0], [19, -1]]
+
+    print(algo.get_convex_indices(points))
+    print(algo.get_convex_points(points))
 
     sys.exit(app.exec_())
--- graham/algorithm.py
+++ graham/algorithm.py
@@ -5,3 +5,18 @@ class GrahamAlgorithm:
 
     def __init__(self):
         print('GrahamAlgorithm init success')
+
+    def get_convex_indices(self, points):
+        return list(range(0, len(points), 2))
+
+    def get_convex_points(self, points):
+        indices = self.get_convex_indices(points)
+        ans = []
+
+        for index in indices:
+            ans.append([points[index][0], points[index][1]])
+
+        return ans
+
+    def get_convex_log(self, points):
+        print('log')
